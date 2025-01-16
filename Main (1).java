0a61b09682b8b6337345bce28913ae8d6f483992
chitrang3467 <168462583+chitrang3467@users.noreply.github.com>
2025-01-16 21:20:45 +0530

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -22,7 +22,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		GetSetname obj1 = new GetSetname();
-		obj1.setName("Vaibhav");
+		obj1.setName("Chitrang");
 		obj1.setAge(20);
 		System.out.println(obj1.getName());
 		System.out.println(obj1.getAge());
